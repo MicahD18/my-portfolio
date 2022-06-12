b5d35c0d1a927bfa51780338b706a361cbe1a157
MicahD18 <micah.daise@nwktc.edu>
2022-06-12 16:51:31 -0600

    finished with navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -12,12 +12,8 @@ import MenuIcon from "@material-ui/icons/Menu";
 import CloseIcon from "@material-ui/icons/Close";
 
 const useStyles = makeStyles((theme) => ({
-  app__navbar: {
-    flexGrow: 1,
-  },
   menu: {
-    marginRight: theme.spacing(2),
-    "@media (min-width: 800px)": {
+    "@media (min-width: 750px)": {
       display: "none",
     },
   },
@@ -29,31 +25,68 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "space-around",
     width: "40vw",
     marginLeft: "15vw",
-    "@media (max-width: 800px)": {
+    "@media (max-width: 750px)": {
       display: "none",
     },
   },
   appBar: {
     backgroundColor: "#F0F0F0",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   app__navbar_logo: {
     width: "225px",
     height: "125px",
   },
   app__navbar_menu: {
-    float: "right",
-    marginTop: "-75px",
-    "@media (min-width: 800px)": {
+    width: "35px",
+    height: "35px",
+    borderRadius: "50%",
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "blue",
+    "@media (min-width: 750px)": {
       display: "none",
     },
   },
-
-  mobileMenuBar: {
-    width: "50vw",
-    height: "50vh",
-    backgroundColor: "red",
-    float: "right",
+  app__navbar_links: {
+    listStyle: "none",
+    margin: 0,
+    padding: 0,
+    height: "100%",
+    width: "100%",
+    display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    marginTop: "75px",
+    marginLeft: "50px",
   },
+  mobile__navbar_menu: {
+    backgroundColor: "#F0F0F0",
+    position: "fixed",
+    float: "right",
+    top: 0,
+    bottom: 0,
+    right: 0,
+    zIndex: 5,
+    padding: "1rem",
+    width: "50%",
+    display: "flex",
+    justifyContent: "flex-end",
+    flexDirection: "column",
+    backgroundSize: "cover",
+    backgroundRepeat: "repeat",
+  },
+  close__button: {
+    width: "35px",
+    height: "35px",
+    color: "black",
+    margin: "0.5rem 1rem",
+  }
 }));
 
 const Navbar = () => {
@@ -62,9 +95,9 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className={classes.app__navbar}>
-      <div position="static" className={classes.appBar}>
-        <Toolbar>
+    <nav>
+      <div >
+        <Toolbar position="static" className={classes.appBar}>
           <img
             src={images.logo}
             alt="logo"
@@ -81,32 +114,46 @@ const Navbar = () => {
               }
             )}
           </div>
+          <div className={classes.app__navbar_menu}>
+            <IconButton className={classes.menu}>
+              <MenuIcon
+                onClick={() => setToggle(true)}
+                style={{ color: "white" }}
+              />
+            </IconButton>
+          </div>
         </Toolbar>
       </div>
 
-      <div position="static" className={classes.app__navbar_menu}>
-        <IconButton className={classes.menu}>
-          <MenuIcon onClick={() => setToggle(true)} />
-        </IconButton>
-
-        {toggle && (
-          <div className={classes.mobileMenuItems}>
-            <IconButton>
-              <CloseIcon onClick={() => setToggle(false)} />
-            </IconButton>
-
+      {toggle && (
+        <div className={classes.mobile__navbar_menu}>
+          <IconButton className={classes.close__button}>
+            <CloseIcon onClick={() => setToggle(false)} />
+          </IconButton>
+          <ul className={classes.app__navbar_links}>
             {["home", "about", "work", "skills", "contact"].map(
               (item, index) => {
                 return (
-                  <li key={index}>
-                    <a>{item}</a>
-                  </li>
+                  <div>
+                    <li
+                      key={index}
+                      style={{
+                        marginBottom: "45px",
+                        cursor: "pointer",
+                        display: "flex",
+                        flexDirection: "column",
+                        fontSize: "18px",
+                      }}
+                    >
+                      <a>{item}</a>
+                    </li>
+                  </div>
                 );
               }
             )}
-          </div>
-        )}
-      </div>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
